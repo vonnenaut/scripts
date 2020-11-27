952cd3cc8c9effd104303ca36b484a3cd6394bae
vonnenaut <danielashcom@gmail.com>
2020-11-27 18:03:20 +0700

    automated sync commit for scripts

--- pull-push_repos.sh
+++ pull-push_repos.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-MESSAGE=$'\n\n\n\nUpdating local repo from remote ...'
+MESSAGE=$'\n\n\n\nPulling changes to local repo from remote ...'
 echo "$MESSAGE"
 source pull_repos.sh
 
-MESSAGE=$'\n\n---------- Committing git repos: ----------'
-echo "$MESSAGE"
+MESSAGE=$'Pull complete.  Begin push ...'
 
 for dir in scripts c_programming learn_c_thw js210 java_masterclass codewars exercism
 do
   cd ~/$dir
-  STR=$'Committing changes to'
+  STR=$'Pushing local changes to'
   echo "$STR $dir ..."
   git add .
   git commit -m "automated sync commit for $dir"
