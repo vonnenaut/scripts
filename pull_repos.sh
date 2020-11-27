d4fe62ed0afec4b08a0d67f8b9c4ef85bada8fe3
vonnenaut <danielashcom@gmail.com>
2020-11-27 17:41:48 +0700

    automated sync commit for scripts

--- pull_repos.sh
+++ pull_repos.sh
@@ -4,7 +4,7 @@
 GREET=$'\n\n---------- Pulling git repos: ----------'
 echo "$GREET"
 
-for dir in scripts c_programming learn_c_thw js210 intro-info-sec java_masterclass codewars exercism
+for dir in scripts c_programming learn_c_thw js210 java_masterclass codewars exercism
 do
   cd ~/$dir
   STR=$'\nChecking for changes to'
