8cfc89a91096e50c308f7a854ca44d3afc8fa642
vonnenaut <danielashcom@gmail.com>
2020-11-27 17:57:07 +0700

    automated sync commit for scripts

--- pull-push_repos.sh
+++ pull-push_repos.sh
@@ -10,10 +10,11 @@ echo "$MESSAGE"
 for dir in scripts c_programming learn_c_thw js210 java_masterclass codewars exercism
 do
   cd ~/$dir
-  STR=$'\n\nCommitting changes to'
+  STR=$'\nCommitting changes to'
   echo "$STR $dir ..."
   git add .
   git commit -m "automated sync commit for $dir"
-  git push  
+  git push
+  ech0 "\n\n"
 done
 
