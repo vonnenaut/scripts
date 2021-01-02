3172caa7f9e582d2b52eef6becbde5f79d3a7fb9
vonnenaut <danielashcom@gmail.com>
2021-01-02 20:00:38 +0700

    removed kandr_c from updating

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in scripts apis_ruby game_designs loo_java_thw kandr-c_prog_lang c_programming learn_c_thw js210 java_masterclass codewars
+for dir in scripts apis_ruby game_designs loo_java_thw c_programming learn_c_thw js210 java_masterclass codewars
 do
   cd ~/$dir
   STR=$'\n\nChecking for changes to'
