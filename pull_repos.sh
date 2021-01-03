c46460fb0abcc76fc78a605ef2896f319903ca4f
vonnenaut <danielashcom@gmail.com>
2021-01-03 15:37:47 +0700

    automated sync commit for scripts

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in scripts apis_ruby game_designs java_crash_course loo_java_thw c_programming learn_c_thw js210 java_masterclass codewars
+for dir in scripts apis_ruby game_designs java_learning c_learning js210 codewars
 do
   cd ~/$dir
   STR=$'\n\nChecking for changes to'
