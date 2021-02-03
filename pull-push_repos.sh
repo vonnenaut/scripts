242486e864cd9e59eddf6ad0da3044ffd4570468
vonnenaut <danielashcom@gmail.com>
2021-02-03 21:44:09 +0700

    automated sync commit for scripts

--- pull-push_repos.sh
+++ pull-push_repos.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-MESSAGE=$'\n\n\n\n########## Pulling changes to local repo from remote ... ##########'
-echo "$MESSAGE"
-source ./scripts/pull_repos.sh
-
-MESSAGE=$'\n\n########## Pull complete.  Begin push ... ##########\n'
+MESSAGE=$'\n\n\n\n########## Checking for remote changes to pull ... ##########'
 echo "$MESSAGE"
 
-for dir in scripts omscs/gios omscs/sad apis_ruby unity_learning game_designs cplusplus_learning java_learning c_learning js210 codewars
+dirArray=(scripts omscs/gios omscs/sad apis_ruby unity_learning game_designs cplusplus_learning java_learning c_learning js210 codewars)
+
+for dir in "${dirArray[@]}" 
+do
+  cd ~/$dir
+  STR=$'\n\nChecking for changes to'
+  echo "$STR $dir ..."
+  git fetch origin
+  reslog=$(git log HEAD..origin/master --oneline)
+  if [[ "${reslog}" != "" ]] ; then
+    git merge origin/master
+  fi
+done
+
+MESSAGE=$'\n\n########## Pull check complete.  Begin check for local changes to push ... ##########\n'
+echo "$MESSAGE"
+
+for dir in "${dirArray[@]}" 
 do
   cd ~/$dir
   STR=$'Pushing local changes to'
--- pull_repos.sh
+++ pull_repos.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-for dir in scripts omscs/gios omscs/sad apis_ruby unity_learning game_designs cplusplus_learning java_learning c_learning js210 codewars
+dirArray=(scripts omscs/gios omscs/sad apis_ruby unity_learning game_designs cplusplus_learning java_learning c_learning js210 codewars)
+
+for dir in "${dirArray[@]}" 
 do
   cd ~/$dir
   STR=$'\n\nChecking for changes to'
   echo "$STR $dir ..."
-  git pull
+  git fetch origin
+  reslog=$(git log HEAD..origin/master --oneline)
+  if [[ "${reslog}" != "" ]] ; then
+    git merge origin/master
+  fi
 done
-
